7d97a11f055652ea99e37a281a1599938a0ef75a
Artem <nitroxess@gmail.com>
2022-12-07 18:26:27 +0300

    try

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,21 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-  entry: "./src/index.js",
+  entry: "./src/script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   template: "index.html",
-    // }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin()
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
@@ -28,6 +30,10 @@ const config = {
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,'css-loader'],
       },
 
       // Add your rules for custom modules here
